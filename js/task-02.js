b72a16eb6e6661aed26817924d3cc3e30ff66d36
VolodymyrBakun <volodymyr.bakun@gmail.com>
2023-04-15 16:39:17 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector("#ingredients")
+
+const list = [];
+
+ingredients.forEach(function createLi (el) { 
+
+const newLi = document.createElement("li");
+newLi.textContent = el;
+newLi.classList.add("item");
+
+list.push(newLi);}
+)
+
+listEl.append(...list)
