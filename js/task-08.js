1cf530e4d2ab0942646bf642ce0e84c792f3605b
VolodymyrBakun <volodymyr.bakun@gmail.com>
2023-04-17 17:29:01 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Всі поля повинні бути заповнені");
+  }
+
+  const result = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(result);
+  event.currentTarget.reset();
+}
