b0859837901de1915c422ce77746e3a87ec4622b
VolodymyrBakun <volodymyr.bakun@gmail.com>
2023-04-17 21:19:33 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+
+  const boxesToCreate = Number(inputEl.value);
+  let box = `<div style="background-color: ${getRandomHexColor()}; width: 30px; height: 30px;"></div>`;
+
+
+  for (let i = 1; i < boxesToCreate; i += 1) {
+    let size = (30 + (i * 10));
+    let color = getRandomHexColor();
+
+    box += `<div style="background-color: ${color}; width: ${size}px; height: ${size}px;"></div>`;
+
+  }
+boxEl.innerHTML = box;
+};
+
+function destroyBoxes() {
+  boxEl.innerHTML = "";
+}
+
+const inputEl = document.querySelector('input')
+const btnCreateEl = document.querySelector('[data-create]');
+const btnDestroyEl = document.querySelector('[data-destroy]');
+const boxEl = document.querySelector('#boxes');
+
+btnCreateEl.addEventListener('click', createBoxes);
+btnDestroyEl.addEventListener('click', destroyBoxes)
